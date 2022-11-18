c84f0162c978338384ca7c5b96116f8cfdbd0f86
Rodrigo-Fialho <rodrigoc_fialho@outlook.com>
2022-11-17 22:54:24 -0300

    first Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import NotificationButton from "./componentes/NotificationButton";
 
 function App() {
-  
   return (
-    <h1>Hello Word</h1>
-  )
+    <>
+      <h1>Hello Word</h1>
+      <NotificationButton/>
+    </>
+  );
 }
 
-export default App
+export default App;
